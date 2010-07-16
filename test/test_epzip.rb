86e098c244840bed42ad6bb7d5f93378b7348434
takahashim <maki@rubycolor.org>
2010-07-16 20:34:30 +0900

    add test

--- test/test_epzip.rb
+++ test/test_epzip.rb
@@ -1,7 +1,9 @@
 require 'helper'
 
 class TestEpzip < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_zip_should_use_existed_dir
+    assert_raise ArgumentError do
+      Epzip.zip("unknown_dir")
+    end
   end
 end
